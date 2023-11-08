400cd45e0b4af67ccf2c5760dbe2c96fb3297701
bibs <bibsbiana@protonmail.com>
2023-11-08 15:39:04 +0100

    Cambio color del título

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,34 @@ import { useState } from 'react';
 function App() {
   const [title, setTitle] = useState('Texto a convertir');
   const [tempTitle, setTempTitle] = useState('');
+  const [color, setColor] = useState('black');
 
   return (
     <>
-      <h1>{title}</h1>
-      <input
-        type="text"
-        value={tempTitle}
-        onChange={(e) => setTempTitle(e.target.value)}
-      />
-      <button onClick={() => {setTitle(tempTitle
-        .split('')
-        .reverse()
-        .join(''));
-        setTempTitle('');
-        }}>
-        Convertit
-      </button>
-      {/* split para separar string en array, le doy la vuelta con reverse y uno con join en string de nuevo */}
-    
+      <div>
+        <h1 style={{color:color}}>{title}</h1>
+        <input
+          type="text"
+          value={tempTitle}
+          onChange={(e) => setTempTitle(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            setTitle(tempTitle.split('').reverse().join(''));
+            {
+              /* split para separar string en array, le doy la vuelta con reverse y uno con join en string de nuevo */
+            }
+            setTempTitle('');
+          }}
+        >
+          Convertit
+        </button>
+      </div>
+      <div>
+        <button onClick={() => setColor('black')}>Negro</button>
+        <button onClick={() => setColor('red')}>Rojo</button>
+        <button onClick={() => setColor('blue')}>Azul</button>
+      </div>
     </>
   );
 }
