cc3a6e38163f23724fd27bc1387a51dec4988f65
bibs <bibsbiana@protonmail.com>
2023-11-08 15:26:36 +0100

    Añado botón

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
   const [title, setTitle] = useState('Texto a convertir');
-    
+  const [tempTitle, setTempTitle] = useState('');
+
   return (
     <>
-        <h1>{title}</h1>
-        <input 
-        type="text" 
-        value={title} 
-        onChange={(e) => setTitle(e.target.value.split('').reverse().join(''))}/>
-        {/* split para separar string en array, le doy la vuelta con reverse y uno con join en string de nuevo */}
+      <h1>{title}</h1>
+      <input
+        type="text"
+        value={tempTitle}
+        onChange={(e) => setTempTitle(e.target.value)}
+      />
+      <button onClick={() => {setTitle(tempTitle
+        .split('')
+        .reverse()
+        .join(''));
+        setTempTitle('');
+        }}>
+        Convertit
+      </button>
+      {/* split para separar string en array, le doy la vuelta con reverse y uno con join en string de nuevo */}
+    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
